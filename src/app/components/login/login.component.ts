086a5c0e78516f9917386a2b16db79bd68c9ed7e
santhosh kumar ravichandran <santhosh.kumar.ravichandran@nl.abnamro.com>
2020-10-28 18:46:41 +0100

    feat: user registration module

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,62 @@
+﻿﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Service } from 'src/app/service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private service: Service
+    ) { }
+
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            name: [''],
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    navigateToRegister(){
+        this.router.navigate(['/register']);
+    }
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.service.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    console.log('login successful');
+                    this.router.navigate(['/shopping']);
+                },
+                error => {
+                    console.log(error)
+                    this.loading = false;
+                });
+    }
+}
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router
+  ) { }
+
+  ngOnInit() {
+      this.form = this.formBuilder.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          username: ['', Validators.required],
+          password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+      if (this.form.invalid) {
+          return;
+      }
+
+      this.loading = true;
+  }
+}
